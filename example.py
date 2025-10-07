67565b5510bface3d3f3d926fc4cf173f462d51c
たか@59期 <takashimizu0529@gmail.com>
2025-10-08 00:39:35 +0900

    提出用に少し整形

--- example.py
+++ example.py
@@ -1,20 +1,24 @@
+# 課題1
+print("hello wprld")
+
+# 課題2
+def greet():
+  print("こんにちは")
+greet()
+
+# 課題3
+def print_name(name):
+  print(f"私の名前は{name}です")
+print_name("たか@59期")
+
+# 課題4
+def get_greet():
+  return "おはようございます"
+print(get_greet())
+
+# 課題5
 def add(a,b):
   return a + b
 a = 1
 b = 3
 print(f"{a} + {b} = {add(a,b)}")
-
-# def get_greet():
-#   return "おはようございます"
-# print(get_greet())
-
-# def print_name(name):
-#   print(f"私の名前は{name}です")
-
-# print_name("たか@59期")
-# def greet():
-#   print("こんにちは")
-
-# greet()
-
-# print("hello wprld")
