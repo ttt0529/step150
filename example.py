7dd684bab40757b6158c0fb69eb237fea4f90e04
たか@59期 <takashimizu0529@gmail.com>
2025-10-07 22:07:49 +0900

    example.pyを追加

---
+++ example.py
@@ -0,0 +1,20 @@
+def add(a,b):
+  return a + b
+a = 1
+b = 3
+print(f"{a} + {b} = {add(a,b)}")
+
+# def get_greet():
+#   return "おはようございます"
+# print(get_greet())
+
+# def print_name(name):
+#   print(f"私の名前は{name}です")
+
+# print_name("たか@59期")
+# def greet():
+#   print("こんにちは")
+
+# greet()
+
+# print("hello wprld")
